34792bf2949bf707f30aaa7c0d43e0b3c42627cd
Alex Miller <alexfeltonmiller@gmail.com>
2019-06-16 14:14:24 -0400

    labels also sorted

--- src/com/dugonggames/elclasico/ImageSorter.java
+++ src/com/dugonggames/elclasico/ImageSorter.java
@@ -1,49 +1,59 @@
 package com.dugonggames.elclasico;
 
 public class ImageSorter {
-    public static void sort(DigitImage[] images, int pixel){
-        sort(images, pixel, 0, images.length-1);
+    public static void sort(DigitImage[] images, int[] labels, int pixel){
+        sort(images, pixel, labels, 0, images.length-1);
     }
 
-    private static void merge(DigitImage[] images, int pixel, int l, int m, int r) {
+    private static void sort(DigitImage[] images, int pixel, int[] labels, int l, int r) {
+        if (l < r) {
+            int m = (l+r)/2;
+            sort(images, pixel, labels, l, m);
+            sort(images, pixel, labels, m+1, r);
+            merge(images, pixel, labels, l, m, r);
+        }
+    }
+
+    private static void merge(DigitImage[] images, int pixel, int[] labels, int l, int m, int r) {
         int n1 = m - l + 1;
         int n2 = r - m;
-        DigitImage[] L = new DigitImage [n1];
-        DigitImage[] R = new DigitImage [n2];
-        for (int i=0; i<n1; ++i)
+        DigitImage[] L = new DigitImage[n1];
+        DigitImage[] R = new DigitImage[n2];
+        int[] ll = new int[n1];
+        int[] rr = new int[n2];
+        for (int i=0; i<n1; ++i){
             L[i] = images[l + i];
-        for (int j=0; j<n2; ++j)
+            ll[i] = labels[l+i];
+        }
+        for (int j=0; j<n2; ++j){
             R[j] = images[m + 1+ j];
+            rr[j] = labels[m + 1+j];
+        }
         int i = 0, j = 0;
         int k = l;
         while (i < n1 && j < n2) {
             if (L[i].compareTo(R[j], pixel) <= 0) {
                 images[k] = L[i];
+                labels[k] = ll[i];
                 i++;
             } else {
                 images[k] = R[j];
+                labels[k] = rr[j];
                 j++;
             }
             k++;
         }
         while (i < n1) {
             images[k] = L[i];
+            labels[k] = ll[i];
             i++;
             k++;
         }
         while (j < n2) {
             images[k] = R[j];
+            labels[k] = rr[j]l
             j++;
             k++;
-        }
-    }
-
-    private static void sort(DigitImage[] images, int pixel, int l, int r) {
-        if (l < r) {
-            int m = (l+r)/2;
-            sort(images, pixel, l, m);
-            sort(images, pixel, m+1, r);
-            merge(images, pixel, l, m, r);
         }
     }
 
